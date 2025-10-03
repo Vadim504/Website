6cc8b41afed1443350ecd8124c3607c61365e032
Vadim <gubaydullin03@internet.ru>
2025-10-03 16:28:38 +0300

    Make migrations and create superuser

--- store/settings.py
+++ store/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store.cast',
+    'store.users',
+    'store.orders',
+    'store.products'
 ]
 
 MIDDLEWARE = [
